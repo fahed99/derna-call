0950774b611cf012ed505fffff083ffef51610e9
Fahed Ben Omran <fahed.benomran@miningchamber.com>
2023-09-20 01:32:45 +0200

    :bug: Fix API bug

--- src/pages/api/aidrequest.ts
+++ src/pages/api/aidrequest.ts
@@ -1,115 +1,137 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-import { PrismaClient } from '@prisma/client';
+import type { NextApiRequest, NextApiResponse } from "next";
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
 const aidRequest = async (req: NextApiRequest, res: NextApiResponse) => {
-  if (req.method === 'POST') {
-    if (req.query.secret !== process.env.AID_REQUEST_TOKEN) {
-      return res.status(401).json({ message: 'Invalid token' });
-    }
+	if (req.method === "POST") {
+		if (req.query.secret !== process.env.AID_REQUEST_TOKEN) {
+			return res.status(401).json({ message: "Invalid token" });
+		}
 
-    const { first_name, address, category, description, familyMembers, phoneNum1, phoneNum2, tweet_id, status } = req.body;
+		const {
+			firstName,
+			address,
+			category,
+			description,
+			familyMembers,
+			phoneNum1,
+			phoneNum2,
+			tweet_id,
+			status,
+		} = req.body;
 
-    const aidRequest = await prisma.aidRequest.create({
-      data: {
-        first_name,
-        address,
-        category,
-        description,
-        date_added: new Date().toISOString(),
-        familyMembers,
-        phoneNum1,
-        phoneNum2,
-        tweet_id,
-        status,
-      },
-    });
+		const aidRequest = await prisma.aidRequest.create({
+			data: {
+				firstName,
+				address,
+				category,
+				description,
+				date_added: new Date().toISOString(),
+				familyMembers,
+				phoneNum1,
+				phoneNum2,
+				tweet_id,
+				status,
+			},
+		});
 
-    return res.status(201).json(aidRequest);
-  }
+		return res.status(201).json(aidRequest);
+	}
 
-  if (req.method === 'GET') {
-    const aidRequests = await prisma.aidRequest.findMany();
-    return res.status(200).json(aidRequests);
-  }
+	if (req.method === "GET") {
+		const aidRequests = await prisma.aidRequest.findMany();
+		return res.status(200).json(aidRequests);
+	}
 
-  if (req.method === 'DELETE') {
-    if (req.query.secret !== process.env.AID_REQUESTS_TOKEN) {
-      return res.status(401).json({ message: 'Invalid token' });
-    }
+	if (req.method === "DELETE") {
+		if (req.query.secret !== process.env.AID_REQUESTS_TOKEN) {
+			return res.status(401).json({ message: "Invalid token" });
+		}
 
-    const { id } = req.query;
+		const { id } = req.query;
 
-    if (!id) {
-      return res.status(422).end('Must Include ID');
-    }
+		if (!id) {
+			return res.status(422).end("Must Include ID");
+		}
 
-    let aidRequest = await prisma.aidRequest.findFirst({
-      where: {
-        id: Number(id),
-      },
-    });
+		let aidRequest = await prisma.aidRequest.findFirst({
+			where: {
+				id: Number(id),
+			},
+		});
 
-    if (!aidRequest) {
-      return res.status(404).end('Aid Request Not Found');
-    }
+		if (!aidRequest) {
+			return res.status(404).end("Aid Request Not Found");
+		}
 
-    aidRequest = await prisma.aidRequest.delete({
-      where: {
-        id: Number(id),
-      },
-    });
+		aidRequest = await prisma.aidRequest.delete({
+			where: {
+				id: Number(id),
+			},
+		});
 
-    return res.json(aidRequest);
-  }
+		return res.json(aidRequest);
+	}
 
-  if (req.method === 'PUT') {
-    if (req.query.secret !== process.env.AID_REQUESTS_TOKEN) {
-      return res.status(401).json({ message: 'Invalid token' });
-    }
+	if (req.method === "PUT") {
+		if (req.query.secret !== process.env.AID_REQUESTS_TOKEN) {
+			return res.status(401).json({ message: "Invalid token" });
+		}
 
-    const { id } = req.query;
+		const { id } = req.query;
 
-    if (!id) {
-      return res.status(422).end('Must Include ID');
-    }
+		if (!id) {
+			return res.status(422).end("Must Include ID");
+		}
 
-    let aidRequest = await prisma.aidRequest.findFirst({
-      where: {
-        id: Number(id),
-      },
-    });
+		let aidRequest = await prisma.aidRequest.findFirst({
+			where: {
+				id: Number(id),
+			},
+		});
 
-    if (!aidRequest) {
-      return res.status(404).end('Aid Request Not Found');
-    }
+		if (!aidRequest) {
+			return res.status(404).end("Aid Request Not Found");
+		}
 
-    const { first_name, address, category, description, familyMembers, date_added, date_resolved, phoneNum1, phoneNum2, tweet_id, status } = req.body;
+		const {
+			firstName,
+			address,
+			category,
+			description,
+			familyMembers,
+			date_added,
+			date_resolved,
+			phoneNum1,
+			phoneNum2,
+			tweet_id,
+			status,
+		} = req.body;
 
-    aidRequest = await prisma.aidRequest.update({
-      where: {
-        id: Number(id),
-      },
-      data: {
-        first_name,
-        address,
-        category,
-        description,
-        familyMembers,
-        date_added,
-        date_resolved,
-        phoneNum1,
-        phoneNum2,
-        tweet_id,
-        status,
-      },
-    });
+		aidRequest = await prisma.aidRequest.update({
+			where: {
+				id: Number(id),
+			},
+			data: {
+				firstName,
+				address,
+				category,
+				description,
+				familyMembers,
+				date_added,
+				date_resolved,
+				phoneNum1,
+				phoneNum2,
+				tweet_id,
+				status,
+			},
+		});
 
-    return res.json(aidRequest);
-  }
+		return res.json(aidRequest);
+	}
 
-  return res.status(405).end(`Method ${req.method} Not Allowed`);
+	return res.status(405).end(`Method ${req.method} Not Allowed`);
 };
 
 export default aidRequest;
--- src/types/AidRequest.ts
+++ src/types/AidRequest.ts
@@ -1,15 +1,14 @@
 export interface AidRequest {
-    id: number;
-    first_name?: string;
-    address?: string;
-    category: string;
-    description: string;
-    date_added: string;
-    date_resolved?: string;
-    familyMembers: number;
-    phoneNum1: number;
-    phoneNum2?: number;
-    tweet_id?: string;
-    status: any;
-  }
-  
+	id: number;
+	first_name?: string;
+	address?: string;
+	category: string;
+	description: string;
+	date_added: string;
+	date_resolved?: string;
+	familyMembers: number;
+	phoneNum1: number;
+	phoneNum2?: number;
+	tweet_id?: string;
+	status: any;
+}
