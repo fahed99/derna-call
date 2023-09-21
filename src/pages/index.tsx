ea1eb53e2491b2cbe2ef196235a2099dddea9052
So <so@miningchamber.com>
2023-09-21 18:59:10 -0400

    :lipstick: Fix logo alignment on large screens

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,11 +5,11 @@ import MainLogo from '@images/main-logo.png';
 
 export default function Home() {
   return (
-    <div className="h-screen text-xl w-screen flex flex-col gap-5 justify-center items-center text-grey-50">
-      <div className="w-[400px] sm:w-[70%] md:w-[50%] lg:w-[35%]">
+    <div className="h-screen w-screen flex flex-col gap-5 justify-center items-center text-grey-50">
+      <div className="w-[400px] sm:w-[70%] md:w-[50%] lg:w-[40%] xl:w-[25%]">
         <Image priority src={MainLogo} alt="Logo" />
       </div>
-      <p className="text-grey-100 w-full rtl text-center">
+      <p className="text-xl text-grey-100 w-[75%] rtl text-center">
         نهدف إلى تسهيل عملية تقديم او الحصول علي دعم لكل اخوتنا واخواتنا في
         مدينة درنة الحبيبة
       </p>
