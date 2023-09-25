4efccb1361352484527f1b524149d2c1bf10f44c
So <so@miningchamber.com>
2023-09-25 04:55:12 -0400

    :lipstick: Improve our button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,68 +1,38 @@
-import { useRequestById } from '@hooks/getRequestByID';
 import { FC } from 'react';
 
 interface Props {
   title: string;
   type: 'primary' | 'secondary' | 'call' | 'check';
+  className?: string;
   requestID?: string;
 }
 
-const LandingCard: FC<Props> = (props) => {
-  const { title, type, requestID } = props;
-  // TODO: This gives a build erorr.
-  // const handleCall = async () => {
-  //   if (requestID) {
-  //     const { data: aidRequest } = useRequestById(requestID);
-  //     if (aidRequest) {
-  //       try {
-  //         const response = await fetch(
-  //           `https://dernacall.ly/api/aidrequest?id=${requestID}`,
-  //           {
-  //             method: 'PATCH',
-  //             headers: {
-  //               'Content-Type': 'application/json'
-  //             },
-  //             body: JSON.stringify({
-  //               callsCount: aidRequest.status + 1
-  //             })
-  //           }
-  //         );
+const Button: FC<Props> = (props) => {
+  const { title, type, className } = props;
 
-  //         if (!response.ok) {
-  //           throw new Error('Failed to update the status');
-  //         }
-  //       } catch (err) {
-  //         throw new Error('err');
-  //       }
-  //     }
-  //   }
-  // };
+  const commonClasses = "shadow-md shadow-grey-50 rounded-3xl bg-primary flex justify-center items-center";
+
+  let specificClasses = "";
+
+  if (type === 'primary') {
+    specificClasses = "h-[50px] w-[200px]";
+  } else if (type === 'call') {
+    specificClasses = "h-[50px] w-[140px]";
+  } else if (type === 'check') {
+    specificClasses = "h-[30px] w-[80px]";
+  } else if (type === 'secondary') {
+    specificClasses = "h-[35px] shadow-lg p-6 w-[100px]";
+  }
+
+  const combinedClasses = `${commonClasses} ${specificClasses} ${className || ''}`;
 
   return (
-    <>
-      {type === 'primary' ? (
-        <div className="h-[50px] shadow-md shadow-grey-50 w-[200px] rounded-3xl bg-primary flex justify-center items-center">
-          <p className="text-white text-xl font-semibold">{title}</p>
-        </div>
-      ) : type === 'call' ? (
-        <div className="h-[50px] shadow-md shadow-grey-50 w-[140px] rounded-3xl bg-primary flex justify-center items-center">
-          <p className="text-white text-base sm:text-lg font-semibold">
-            {title}
-          </p>
-        </div>
-      ) : type === 'check' ? (
-        <div className="h-[30px] shadow-md shadow-grey-50 w-[80px] rounded-3xl bg-primary flex justify-center items-center">
-          <p className="text-white text-base sm:text-lg font-semibold">
-            {title}
-          </p>
-        </div>
-      ) : (
-        <div className="h-[35px] shadow-lg p-6 shadow-grey-50 w-[100px] rounded-xl bg-primary flex justify-center items-center">
-          <p className="text-white text-md font-semibold">{title}</p>
-        </div>
-      )}
-    </>
+    <div className={combinedClasses}>
+      <p className={`text-white ${type === 'primary' || type === 'secondary' ? 'text-xl' : 'text-base sm:text-lg'} font-semibold`}>
+        {title}
+      </p>
+    </div>
   );
 };
 
-export default LandingCard;
+export default Button;
